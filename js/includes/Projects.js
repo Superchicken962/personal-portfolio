278d8563c3ce055095a22e0fb4f586a7393cf420
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2024-01-11 01:53:12 +1030

    Actually fixed images not loading
    Reversed last change, and just made sure the projects specify the correct file extension

--- js/includes/Projects.js
+++ js/includes/Projects.js
@@ -15,8 +15,8 @@ const Projects = [
             "C#"
         ],
         "assets": [
-            {"desc": "Vehicle page where the trip info from in game is shown", "filename": "omsiwebserver_image1.png"},
-            {"desc": "Page showing the trips that the player has driven in game, all stored in a JSON file.", "filename": "omsiwebserver_image2.png"}
+            {"desc": "Vehicle page where the trip info from in game is shown", "filename": "omsiwebserver_image1.PNG"},
+            {"desc": "Page showing the trips that the player has driven in game, all stored in a JSON file.", "filename": "omsiwebserver_image2.PNG"}
         ],
         // "github": "https://github.com/Superchicken962/OMSIWebServer",
         "private": true
@@ -34,8 +34,8 @@ const Projects = [
             "Node.js",
         ],
         "assets": [
-            {"desc": "All previous workouts shown", "filename": "gymworkoutwebsite_image1.png"},
-            {"desc": "View/edit specific workout", "filename": "gymworkoutwebsite_image2.png"},
+            {"desc": "All previous workouts shown", "filename": "gymworkoutwebsite_image1.PNG"},
+            {"desc": "View/edit specific workout", "filename": "gymworkoutwebsite_image2.PNG"},
         ],
         "private": true
     },
@@ -52,13 +52,13 @@ const Projects = [
             "Node.js",
         ],
         "assets": [
-            {"desc": "Store where cases can be bought with fake money", "filename": "tf2unboxsim_image1.png"},
-            {"desc": "Shopping cart where with items from store", "filename": "tf2unboxsim_image2.png"},
-            {"desc": "Page for adding fake money, clicking the button increases the balance instantly.", "filename": "tf2unboxsim_image3.png"},
-            {"desc": "The backpack - where all bought/found items are stored.", "filename": "tf2unboxsim_image4.png"},
-            {"desc": "Recreation of the 'unboxing' from Team Fortress 2 using a GIF.", "filename": "tf2unboxsim_image5.png"},
-            {"desc": "Page to easily add new cases to the website (Authorised access only).", "filename": "tf2unboxsim_image6.png"},
-            {"desc": "Web scraping the TF2 Wiki to find items in the case (Authorised access only).", "filename": "tf2unboxsim_image7.png"},
+            {"desc": "Store where cases can be bought with fake money", "filename": "tf2unboxsim_image1.PNG"},
+            {"desc": "Shopping cart where with items from store", "filename": "tf2unboxsim_image2.PNG"},
+            {"desc": "Page for adding fake money, clicking the button increases the balance instantly.", "filename": "tf2unboxsim_image3.PNG"},
+            {"desc": "The backpack - where all bought/found items are stored.", "filename": "tf2unboxsim_image4.PNG"},
+            {"desc": "Recreation of the 'unboxing' from Team Fortress 2 using a GIF.", "filename": "tf2unboxsim_image5.PNG"},
+            {"desc": "Page to easily add new cases to the website (Authorised access only).", "filename": "tf2unboxsim_image6.PNG"},
+            {"desc": "Web scraping the TF2 Wiki to find items in the case (Authorised access only).", "filename": "tf2unboxsim_image7.PNG"},
         ],
         // "github": "https://github.com/Superchicken962/tf2unboxsimulator",
         // "viewonline": "http://tf2unboxsimulator.uk.to/",
@@ -81,7 +81,7 @@ const Projects = [
             "Node.js",
         ],
         "assets": [
-            {"desc": "Discord webhook messages sent with information from the minecraft server", "filename": "minecraftplugin_image1.png"},
+            {"desc": "Discord webhook messages sent with information from the minecraft server", "filename": "minecraftplugin_image1.PNG"},
         ],
         "private": true
     },
@@ -100,14 +100,14 @@ const Projects = [
             "Node.js",
         ],
         "assets": [
-            {"desc": "List of vehicles in service", "filename": "adelaidebuswebsite_image1.png"},
-            {"desc": "Map of vehicles in service", "filename": "adelaidebuswebsite_image1_2.png"},
-            {"desc": "List of stops within 1km of geolocation (alternatively, search bar can be used)", "filename": "adelaidebuswebsite_image2.png"},
-            {"desc": "Stop information with next & previous arrivals and any service alerts that may apply to a route stopping there", "filename": "adelaidebuswebsite_image3.png"},
-            {"desc": "Favourite stops and vehicles for logged in users", "filename": "adelaidebuswebsite_image4.png"},
-            {"desc": "'Last seen' vehicle information - vehicle positions are stored in database when in service, then retrieved if not in service when checking page", "filename": "adelaidebuswebsite_image5.png"},
-            {"desc": "Trip information, including estimated next services as well as the vehicles that previously serviced the trip", "filename": "adelaidebuswebsite_image6.png"},
-            {"desc": "Live vehicle information - show vehicle position on map, with the next stops and it's punctuality", "filename": "adelaidebuswebsite_image7.png"},
+            {"desc": "List of vehicles in service", "filename": "adelaidebuswebsite_image1.PNG"},
+            {"desc": "Map of vehicles in service", "filename": "adelaidebuswebsite_image1_2.PNG"},
+            {"desc": "List of stops within 1km of geolocation (alternatively, search bar can be used)", "filename": "adelaidebuswebsite_image2.PNG"},
+            {"desc": "Stop information with next & previous arrivals and any service alerts that may apply to a route stopping there", "filename": "adelaidebuswebsite_image3.PNG"},
+            {"desc": "Favourite stops and vehicles for logged in users", "filename": "adelaidebuswebsite_image4.PNG"},
+            {"desc": "'Last seen' vehicle information - vehicle positions are stored in database when in service, then retrieved if not in service when checking page", "filename": "adelaidebuswebsite_image5.PNG"},
+            {"desc": "Trip information, including estimated next services as well as the vehicles that previously serviced the trip", "filename": "adelaidebuswebsite_image6.PNG"},
+            {"desc": "Live vehicle information - show vehicle position on map, with the next stops and it's punctuality", "filename": "adelaidebuswebsite_image7.PNG"},
         ],
         "private": true
     },
@@ -126,7 +126,7 @@ const Projects = [
             "Node.js",
         ],
         "assets": [
-            {"desc": "Album view - list all songs in the album, and show current song progress on bottom bar along with pause & skip buttons and a volume slider", "filename": "localmusicplayer_image1.png"},
+            {"desc": "Album view - list all songs in the album, and show current song progress on bottom bar along with pause & skip buttons and a volume slider", "filename": "localmusicplayer_image1.PNG"},
         ],
         "private": true
     },
@@ -143,11 +143,11 @@ const Projects = [
             "Node.js",
         ],
         "assets": [
-            {"desc": "All saved games of Dead By Daylight, with 'at a glance' stats and grouped by date", "filename": "dbdstattracker_image1.png"},
-            {"desc": "Manually recording the game data - choosing killer, survivors, and timing the match and more", "filename": "dbdstattracker_image2.png"},
-            {"desc": "Map specific data - shows the official description of the map and lists every game recorded in that map", "filename": "dbdstattracker_image3.png"},
-            {"desc": "Killer specific stats - stats for games only against a specified killer", "filename": "dbdstattracker_image4.png"},
-            {"desc": "Games summary - statistics/summary of all games", "filename": "dbdstattracker_image5.png"},
+            {"desc": "All saved games of Dead By Daylight, with 'at a glance' stats and grouped by date", "filename": "dbdstattracker_image1.PNG"},
+            {"desc": "Manually recording the game data - choosing killer, survivors, and timing the match and more", "filename": "dbdstattracker_image2.PNG"},
+            {"desc": "Map specific data - shows the official description of the map and lists every game recorded in that map", "filename": "dbdstattracker_image3.PNG"},
+            {"desc": "Killer specific stats - stats for games only against a specified killer", "filename": "dbdstattracker_image4.PNG"},
+            {"desc": "Games summary - statistics/summary of all games", "filename": "dbdstattracker_image5.PNG"},
         ],
         // "github": "https://github.com/Superchicken962/dbd-manual-stats",
         "private": true
