55dd2e9b4cb772fc7de92a4ea03fc68534871d8b
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2024-04-20 06:22:11 +0930

    new theme
    limited time theme with my cursor drawn font

--- js/footer.js
+++ js/footer.js
@@ -108,6 +108,9 @@ function loadTheme() {
             }
             
             break;
+        
+        case "basic":
+            root.style.setProperty("--font", "cursor");
 
         default:
         case "dark":
