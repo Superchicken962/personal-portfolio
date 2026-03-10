5cd285944c82a445aabf87fcca0c70808d43537f
mark <58424347+Superchicken962@users.noreply.github.com>
2026-03-10 17:43:11 +1030

    Disable timeline page from home page

--- js/includes/Pages.js
+++ js/includes/Pages.js
@@ -4,9 +4,9 @@ const Pages = [
         "description": "Some of my programming projects that I feel are best representative of my skills, and overall learning journey!",
         "file": "projects.html"
     },
-    {
-        "title": "Timeline",
-        "description": "Timeline of my journey in programming throughout the years, plus big projects I made.<br><br>Coming Soon...",
-        // "file": "timeline.html"
-    }
+    //{
+    //    "title": "Timeline",
+    //    "description": "Timeline of my journey in programming throughout the years, plus big projects I made.<br><br>Coming Soon...",
+    //    // "file": "timeline.html"
+    //}
 ];
